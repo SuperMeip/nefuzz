e744c02f195f01b08ac8974a3ef55a9b5cdb9a12
Meep <supermeip@gmail.com>
2016-11-04 13:10:53 -0400

    changed Time to time

--- nefuzzbot.py
+++ nefuzzbot.py
@@ -193,7 +193,7 @@ class Event:
             self.time = Time.replace(tzinfo=None)
         else:
             self.time = Time
-        if(self.Time < datetime(datetime.now().year, 3, 13, 2)  ) or (self.time > datetime(datetime.now().year, 11, 6, 2)):
+        if(self.time < datetime(datetime.now().year, 3, 13, 2)  ) or (self.time > datetime(datetime.now().year, 11, 6, 2)):
             self.time.hour -= 1
         self.location = Location
         self.host = Host
